102489ace4cd52bbd997e5bfb49d9e36c9bea0a0
Ta Thien Phuoc <tathienphuoc@gmail.com>
2021-12-06 23:16:00 +0700

    Candidate one2many Attendance

--- src/main/java/SGU/Engrisk/DTO/Candidate/ResponseCandidateDTO.java
+++ src/main/java/SGU/Engrisk/DTO/Candidate/ResponseCandidateDTO.java
@@ -1,5 +1,6 @@
 package SGU.Engrisk.DTO.Candidate;
 
+import SGU.Engrisk.DTO.Exam.ResponseExamDTO;
 import SGU.Engrisk.Models.Attendance;
 import SGU.Engrisk.Models.Candidate;
 import lombok.Data;
@@ -10,21 +11,10 @@ import java.util.stream.Collectors;
 
 @Data
 public class ResponseCandidateDTO extends UpdateCandidateDTO {
-//    private String attendanceIds;
-//
-//    public void setAttendanceIds(Candidate candidate) {
-//        List<Attendance> attendances = candidate.getAttendances();
-//        if (attendances == null) {
-//            this.attendanceIds = "";
-//            return;
-//        }
-//        this.attendanceIds = candidate.getAttendances().stream().map(attendance -> String.valueOf(attendance.getId())).collect(Collectors.joining(","));
-//    }
-
+    private List<ResponseAttendanceDTO> attendances;
     public static ResponseCandidateDTO convert(Candidate candidate) {
         ResponseCandidateDTO res = (new ModelMapper()).map(candidate, ResponseCandidateDTO.class);
-
-//        res.setAttendanceIds(candidate);
+        res.setAttendances(candidate.getAttendances().stream().map(c -> ResponseAttendanceDTO.convert(c)).collect(Collectors.toList()));
         return res;
     }
 }
--- src/main/java/SGU/Engrisk/DTO/Exam/ResponseExamDTO.java
+++ src/main/java/SGU/Engrisk/DTO/Exam/ResponseExamDTO.java
@@ -1,6 +1,9 @@
 package SGU.Engrisk.DTO.Exam;
 
+//import SGU.Engrisk.DTO.Attendance.ResponseAttendanceRef;
+import SGU.Engrisk.DTO.Exam.ResponseExamDTO;
 import SGU.Engrisk.Models.Attendance;
+import SGU.Engrisk.Models.Exam;
 import SGU.Engrisk.Models.Exam;
 import SGU.Engrisk.Models.Room;
 import lombok.Data;
@@ -11,23 +14,11 @@ import java.util.stream.Collectors;
 
 @Data
 public class ResponseExamDTO extends UpdateExamDTO {
-//    private String roomIds;
-//    private String attendanceIds;
+//    private List<ResponseAttendanceRef> attendances;
+
     public static ResponseExamDTO convert(Exam exam) {
         ResponseExamDTO res = (new ModelMapper()).map(exam, ResponseExamDTO.class);
-
-//        List<Room> rooms = exam.getRooms();
-//        List<Attendance> attendances = exam.getAttendances();
-//        if (attendances == null)
-//            res.setAttendanceIds("");
-//        else
-//            res.setAttendanceIds(exam.getAttendances().stream().map(attendance -> String.valueOf(attendance.getId())).collect(Collectors.joining(",")));
-//
-//        if (rooms == null)
-//            res.setRoomIds("");
-//        else
-//            res.setRoomIds(exam.getRooms().stream().map(r -> String.valueOf(r.getId())).collect(Collectors.joining(",")));
-
+//        res.setAttendances(exam.getAttendances().stream().map(e -> ResponseAttendanceRef.convert(e)).collect(Collectors.toList()));
         return res;
     }
 }
