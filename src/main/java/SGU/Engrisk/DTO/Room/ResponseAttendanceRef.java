34eb0054bc14eaf81c342d11c9a9ee85d8df7907
Ta Thien Phuoc <tathienphuoc@gmail.com>
2022-01-04 14:19:57 +0700

    Fix: Return candidate

--- src/main/java/SGU/Engrisk/DTO/Room/ResponseAttendanceRef.java
+++ src/main/java/SGU/Engrisk/DTO/Room/ResponseAttendanceRef.java
@@ -1,6 +1,6 @@
 package SGU.Engrisk.DTO.Room;
 
-import SGU.Engrisk.DTO.Candidate.ResponseExamRef;
+import SGU.Engrisk.DTO.Exam.ResponseCandidateRef;
 import SGU.Engrisk.Models.Attendance;
 import SGU.Engrisk.Models.AttendanceID;
 import lombok.Data;
@@ -10,6 +10,8 @@ import org.modelmapper.ModelMapper;
 public class ResponseAttendanceRef {
     private AttendanceID id;
 
+    private ResponseCandidateRef candidate;
+
     private Float listening;
     private Float speaking;
     private Float reading;
@@ -18,6 +20,7 @@ public class ResponseAttendanceRef {
 
     public static ResponseAttendanceRef convert(Attendance attendance) {
         ResponseAttendanceRef res = (new ModelMapper()).map(attendance, ResponseAttendanceRef.class);
+        res.setCandidate(ResponseCandidateRef.convert(attendance.getCandidate()));
         return res;
     }
 }
